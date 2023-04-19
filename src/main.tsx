93757448d68146cafb8e6e1fec237d4ca30f7f58
thesimpleGithub <thesimple369@gmail.com>
2023-04-20 06:56:12 +0900

    add baseurl

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import GlobalStyle from './styles/global';
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/mycolor-admin">
     <GlobalStyle />
     <App />
   </BrowserRouter>,
